390ed95d76b4b341663bbf05d0a53cf91ad3d205
Marc <53114166+bymarcx@users.noreply.github.com>
2020-08-11 22:59:22 +0200

    add bootstrap

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-/*
- * Place your custom JavaScript in here!
- *
- * (C) BYMARC
- *
- */
--- src/js/components/custom.js
+++ src/js/components/custom.js
@@ -1,2 +1,13 @@
+/*
+ * Place your custom JavaScript in here!
+ *
+ * (C) BYMARC
+ *
+ */
+
+import 'jquery';
+import 'popper.js'
+import 'bootstrap';
+
 console.log('custom')
 console.log('hallo')
