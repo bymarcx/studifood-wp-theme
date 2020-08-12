9592029d4e4dfded260763325b8f6fa3cd57792c
Marc <53114166+bymarcx@users.noreply.github.com>
2020-08-12 22:10:37 +0200

    custom wp- login page

--- functions.php
+++ functions.php
@@ -60,3 +60,15 @@ function _customtheme_widgets_init() {
 
 // INCLUDE CUSTOMIZER
 // require get_template_directory() . '/inc/customizer.php';
+
+
+
+function _customtheme_login_header_url( $login_header_url ) {
+    return '/';;
+}
+add_filter( 'login_headerurl', '_customtheme_login_header_url' );
+
+function _customtheme_login_footer() {
+    echo '<p style="text-align: center; margin-top: 30px">&copy; 2020 '; bloginfo( 'name' ); echo '</a> | All rights reserved.</p>';
+}
+add_action( 'login_footer', '_customtheme_login_footer' );
