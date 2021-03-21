b9e4482343542ee2e2078515562df343e4ffef5a
Marc <marcf.eberhard@gmail.com>
2021-03-21 18:42:55 +0100

    fixed bugs on firefox

--- 404.php
+++ 404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the 404-page.
  *
@@ -9,19 +10,47 @@
 
 <?php get_header(); ?>
 
-    <section id="404">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12">
+<section class="stage small">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <?php get_template_part('template-parts/stage/headerbar') ?>
 
-                    <h1>Error <span style="font-weight: 900">404</span></h1>
-                    <h2>Upps, diese Seite existiert leider nicht..</h2>
-                    <br>
-                    <a href="/" class="btn btn-primary">zur Startseite!</a>
+                <div class="image">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <h1 class="h1-recipe"><span>Error 404</span></h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="img_overlay"></div>
+                    <img src="<?= get_template_directory_uri() ?>/dist/img/footer_bg.jpg" />
 
                 </div>
+
+
+
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-<?php get_header(); ?>
+<?php get_template_part('template-parts/content/breadcrumb') ?>
+
+<section id="page404">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h2>Upps, diese Seite existiert leider nicht.</h2>
+                <p> Die gewünschte Seite konnte leider nicht gefunden werden. </p>
+                <p>
+                    <a href="/" class="btn btn-primary">zur Startseite!</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
