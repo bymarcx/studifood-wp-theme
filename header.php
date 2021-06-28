a28322f81d2ead91dca3c53d6345a660771e6dfc
Marc <marcf.eberhard@gmail.com>
2021-06-28 23:21:11 +0200

    GO-LIVE settings - Cookiebot

--- header.php
+++ header.php
@@ -30,32 +30,40 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <script type="text/javascript">
+        function CookiebotCallback_OnDecline() {
+            if (Cookiebot.changed) {
+                document.location.reload();
+            }
+        }
+    </script>
+
     <?php wp_head(); ?>
 </head>
 
 <body id="top">
-<div id="scroll-container">
+    <div id="scroll-container">
 
-    <?php get_template_part('template-parts/stage/headerbar') ?>
+        <?php get_template_part('template-parts/stage/headerbar') ?>
 
 
-    <div class="scroll-bar-outer" id="scroll-bar-outer-id">
-        <div id="scroll-bar"></div>
-    </div>
+        <div class="scroll-bar-outer" id="scroll-bar-outer-id">
+            <div id="scroll-bar"></div>
+        </div>
 
-    <nav class="nav">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'primary',
-                        'container' => 'nav-buttons',
-                    ));
-                    ?>
+        <nav class="nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'primary',
+                            'container' => 'nav-buttons',
+                        ));
+                        ?>
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    <main>
+        <main>
